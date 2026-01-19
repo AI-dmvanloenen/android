77be42452b139336b8d6ab618c451ba27cbc705d
Douwe van Loenen <douwe@advanceinsight.dev>
2026-01-19 14:20:30 +0100

    Fix tree

--- android_api/__manifest__.py
+++ android_api/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Android API',
-    'version': '18.0.1.0.0',
+    'version': '18.0.1.0.1',
     'category': 'Technical',
     'summary': 'REST API endpoints for Android app integration',
     'description': """
